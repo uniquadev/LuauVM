dd396e30a4303ca1c681ca6473861e8540de58d9
uniquadev <uniquadev@gmail.com>
2022-07-23 23:29:39 +0100

    commit

---
+++ MakeTests/main.py
@@ -0,0 +1,35 @@
+# imports
+from asyncio import subprocess
+import asyncio
+import os
+
+# read MakeTests/template.luau as TEMPLATE
+TEMPLATE = ""
+with open("MakeTests/template.luau", "r") as f:
+    TEMPLATE = f.read()
+
+# write a luau test file inside Tests folder
+async def make_test(file:str):
+    # luau compile the test chunk
+    proc = await subprocess.create_subprocess_shell(
+        f'luau --compile=binary MakeTests/src/{file}', stdout=subprocess.PIPE
+    )
+    # read stdout and get the bytecode
+    bytecode = await proc.communicate()
+    bytecode = bytecode[0].__str__()[1:]
+    # write the bytecode inside the test file
+    with open("Tests/" + file, "w+") as f:
+        src = TEMPLATE.replace('"{bytecode}"', bytecode)
+        src = src.replace('{file}', file)
+        f.write(src)
+
+# exec "luau --compile=binary MakeTests\scripts\* >> Tests\*.luau"
+def main():
+    # loop trought src folder
+    for file in os.listdir("MakeTests/src"):
+        # perform the asyncio task
+        asyncio.run(make_test(file))
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,25 @@
+LuauVM <img src="https://raw.githubusercontent.com/Roblox/luau/master/docs/logo.svg" height="40">
+====
+LuauVM (lowercase u, /ˈlu.aʊ/) is a fast and small [luau](https://github.com/Roblox/luau) interpreter wrote in luau. It aim to provide sandboxing, arbitrary code execution and "obfuscation" templates.
+
+This software can be used as study material for luau - lua 5.x internals. 
+
+# Usage
+```lua
+local vm = require("./VM/main");
+local bytecode = [[BYTECODE]]
+
+-- #1
+local proto = vm.luau_load(bytecode);
+local closure = vm.wrap_proto(proto);
+closure();
+
+-- # 2
+local closure = vm.loadstring(bytecode);
+closure();
+```
+
+> **Note:** only an interpreter is provided and compiled code must be obtained from a supported luau compiler
+
+# License ⚖
+[MIT LICENSE](https://github.com/uniquadev/GuiToLuaConverter/LICENSE.txt)
